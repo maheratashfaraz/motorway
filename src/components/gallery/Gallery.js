47eb4e29e16e96590909126aac8575dfa5122d47
Maher Atashfaraz <maheratashfaraz@Mahers-MBP.broadband>
2021-10-11 16:50:35 +0100

    Add lazyloading for loading images

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './Gallery.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Gallery = (props) => {
     const { images } = props
@@ -15,15 +17,15 @@ const Gallery = (props) => {
     return (
         <>
             <div class={model ? "model open" : "model"}>
-                <img src={temoImgSrc} />
+                <img alt='' src={temoImgSrc} />
                 <button class="close" onClick={() => setModel(false)} />
             </div>
             <div class="gallery">
                 {images && images.map(img => {
                     return (
                         <div class="pics" key={img.id}>
-                            <img src={`${img.url}.jpg`} alt='' style={{ width: '100%' }} onClick={() => getImg(`${img.url}.jpg`)} />
-                            <img src={`${img.user.profile_image}.webp`} alt='' style={{ width: '100%' }} onClick={() => getImg(`${img.user.profile_image}.webp`)} />
+                            <LazyLoadImage effect='blur' src={`${img.url}.jpg`} alt='' width="100%" onClick={() => getImg(`${img.url}.jpg`)} />
+                            <LazyLoadImage effect='blur' src={`${img.user.profile_image}.webp`} alt='' width="100%" onClick={() => getImg(`${img.user.profile_image}.webp`)} />
                         </div>
                     )
                 })}
